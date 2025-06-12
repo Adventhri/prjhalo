7951811fe2149e34a59a331d18130cb5763700f6
adventhri <adventhricatelo55@gmail.com>
2025-06-12 10:53:02 +0700

    memasukan data 5 buah nama  ke dalam array string

--- halo.cpp
+++ halo.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main() {
-    string nama[5]; // Inisialisasi array string ukuran 5
+    string nama[5];
+
+    nama[0] = "rayhan";
+    nama[1] = "fadli";
+    nama[2] = "ijan";
+    nama[3] = "raka";
+    nama[4] = "anshari";
+
     return 0;
 }
+
