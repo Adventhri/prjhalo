b9909be6599f050890456a931e9f62e2371e6cfd
adventhri <adventhricatelo55@gmail.com>
2025-06-25 13:34:02 +0700

    Menambahkan struct Pesan dan pointer head untuk linked list

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,13 +1,9 @@
-void tambahPesan(string isi) {
-    Pesan* baru = new Pesan{isi, NULL};
-    if (head == NULL) {
-        head = baru;
-    } else {
-        Pesan* temp = head;
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = baru;
-    }
-    cout << "Pesan ditambahkan: \"" << isi << "\"\n";
-}
+#include <iostream>
+using namespace std;
+
+struct Pesan {
+    string isi;
+    Pesan* next;
+};
+
+Pesan* head = NULL;
